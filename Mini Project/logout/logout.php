6ad1703b3b17088450b842795e703393d8ef3422
sahanaganesh <76624585+sahanaganesh@users.noreply.github.com>
2021-08-05 23:27:43 +0530

    Add files via upload

---
+++ Mini Project/logout/logout.php
@@ -0,0 +1,6 @@
+<?php
+require_once('C:\xampp\htdocs\Mini Project\database scripts\dbconnector.php');
+session_destroy();
+header('Location:http://localhost/Mini%20Project/login%20page/frontend/login.php');
+?>
+
